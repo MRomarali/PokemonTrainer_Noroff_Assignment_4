fcffa90e42fceff694b695ab06f29d2a39897e03
Cusatelli <einarvandevelde@gmail.com>
2022-02-03 16:34:01 +0100

    feat(Routing): added AuthGuard & Service to routes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
+import { AuthService } from './service/auth.service';
+import { GuardedComponent } from './components/guarded/guarded.component';
+import { AuthGuard } from './components/auth/auth.guard';
 
 const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'login', component: LoginComponent },
+    { path: 'guarded', component: GuardedComponent, canActivate: [AuthGuard] },
 ];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
+    exports: [RouterModule],
+    providers: [AuthService]
 })
 export class AppRoutingModule { }

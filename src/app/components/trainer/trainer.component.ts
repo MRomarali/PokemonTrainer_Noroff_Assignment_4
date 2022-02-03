f10f0e22153b17fdd8dbd69d5c2db3da9d05583a
Cusatelli <einarvandevelde@gmail.com>
2022-02-03 20:27:47 +0100

    feat: add on button click event to remove pokemon from collection (WIP).

--- src/app/components/trainer/trainer.component.ts
+++ src/app/components/trainer/trainer.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { CollectionService } from 'src/app/service/collection.service';
 
 @Component({
   selector: 'app-trainer',
   templateUrl: './trainer.component.html',
   styleUrls: ['./trainer.component.scss']
 })
-export class TrainerComponent implements OnInit {
+export class TrainerComponent {
+  constructor(private collectionService: CollectionService) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  public onRemoveClicked(id: number): void {
+    if(confirm('Are you sure you wish to delete this pokemon?')) {
+      this.collectionService.removeFromCollection(id);
+    }
   }
-
 }

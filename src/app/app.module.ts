43da5147ffe05edc57874c68e3cf37fdc3b56127
OmarAbdiAli <omarabdi.ali@se.experis.com>
2022-02-03 08:08:44 +0100

    fix: added pages to the module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { TrainerPageComponent } from './trainer-page/trainer-page.component';
+import { CataloguePageComponent } from './catalogue-page/catalogue-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    CataloguePageComponent,
+    TrainerPageComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]

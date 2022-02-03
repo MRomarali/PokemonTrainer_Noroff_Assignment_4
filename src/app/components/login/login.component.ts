c7d7994da5c0e594776b60ac902f627cab54a760
Cusatelli <einarvandevelde@gmail.com>
2022-02-03 14:07:17 +0100

    feat(Login): added simple login to LocalStorage.

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+const STORAGE_KEY = 'trainer';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup = new FormGroup({
+    trainerName: new FormControl(''),
+  });
+  submitted = false;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      username: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(42)
+        ]
+      ]
+    });
+  }
+
+  get getFormData(): { [key: string]: AbstractControl } {
+    return this.loginForm.controls;
+  }
+
+  onSubmit(): void {
+    this.submitted = true;
+
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    const username = this.loginForm.value;
+    console.log(STORAGE_KEY, JSON.stringify(username, null, 2));
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(username));
+  }
+
+  onReset(): void {
+    this.submitted = false;
+    this.loginForm.reset();
+  }
+}

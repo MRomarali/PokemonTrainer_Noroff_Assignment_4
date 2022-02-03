c8791a09e95840831321f3b43eb8e3e06bd3ef40
Cusatelli <einarvandevelde@gmail.com>
2022-02-03 16:30:07 +0100

    chore(Page): added a temporary guarded page.

---
+++ src/app/components/guarded/guarded.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-guarded',
+  templateUrl: './guarded.component.html',
+  styleUrls: ['./guarded.component.scss']
+})
+export class GuardedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

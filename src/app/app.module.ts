edc53b3a09a2befff43a73a143b6ee8fb54b3c36
Cusatelli <einarvandevelde@gmail.com>
2022-02-03 16:30:27 +0100

    chore(Page): added a temporary guarded page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HomeComponent } from './components/home/home.component';
+import { GuardedComponent } from './components/guarded/guarded.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    HomeComponent
+    HomeComponent,
+    GuardedComponent
   ],
   imports: [
     BrowserModule,

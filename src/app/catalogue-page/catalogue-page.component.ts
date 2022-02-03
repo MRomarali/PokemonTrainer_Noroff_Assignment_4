65ed42a4de7f7dfa8873eda68d840aaa928e4b36
OmarAbdiAli <omarabdi.ali@se.experis.com>
2022-02-03 08:10:17 +0100

    fix: added components and files to the project

---
+++ src/app/catalogue-page/catalogue-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-catalogue-page',
+  templateUrl: './catalogue-page.component.html',
+  styleUrls: ['./catalogue-page.component.scss']
+})
+export class CataloguePageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/trainer-page/trainer-page.component.ts
@@ -0,0 +1,13 @@
+import { Pokemon } from 'src/app/models/pokemon.model';
+import { PokemonService } from './../../services/pokemon.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-trainer-page',
+  templateUrl: './trainer-page.component.html',
+  styleUrls: ['./trainer-page.component.scss']
+})
+export class TrainerPageComponent {
+
+
+}

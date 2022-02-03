197c8aada51e3e05f38a9d4657f7e35f35e7f92f
Cusatelli <einarvandevelde@gmail.com>
2022-02-03 16:52:34 +0100

    refactor(Page): move guarded to catalogue page.

--- src/app/components/catalogue/catalogue.component.spec.ts
+++ src/app/components/catalogue/catalogue.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { GuardedComponent } from './guarded.component';
+import { CatalogueComponent } from './catalogue.component';
 
-describe('GuardedComponent', () => {
-  let component: GuardedComponent;
-  let fixture: ComponentFixture<GuardedComponent>;
+describe('CatalogueComponent', () => {
+  let component: CatalogueComponent;
+  let fixture: ComponentFixture<CatalogueComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ GuardedComponent ]
+      declarations: [ CatalogueComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(GuardedComponent);
+    fixture = TestBed.createComponent(CatalogueComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });

de7b133e133619a20cd3a5e729057d6c9bddd746
Cusatelli <einarvandevelde@gmail.com>
2022-02-04 13:49:20 +0100

    feat(Collection): implemented collection of pokemons in trainer page.

--- src/app/components/trainer/trainer.component.ts
+++ src/app/components/trainer/trainer.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { STORAGE_TRAINER_KEY } from 'src/app/constants';
+import { Pokemon } from 'src/app/models/pokemon.model';
 import { CollectionService } from 'src/app/service/collection.service';
 
 @Component({
@@ -7,11 +9,25 @@ import { CollectionService } from 'src/app/service/collection.service';
   styleUrls: ['./trainer.component.scss']
 })
 export class TrainerComponent {
+  collection: Pokemon[] = [];
+
   constructor(private collectionService: CollectionService) { }
 
+  ngOnInit(): void {
+    this.update();
+  }
+
+  private update(): void {
+    const data: string | null = localStorage.getItem(STORAGE_TRAINER_KEY);
+    const collection = data ? data : 'null';
+
+    this.collection = JSON.parse(collection).collection;
+  }
+
   public onRemoveClicked(id: number): void {
-    if(confirm('Are you sure you wish to delete this pokemon?')) {
+    if (confirm('Are you sure you wish to delete this pokemon?')) {
       this.collectionService.removeFromCollection(id);
+      this.update();
     }
   }
 }

e2bc31cd3e92092af24be4cbed6b906f04a6f210
Cusatelli <einarvandevelde@gmail.com>
2022-02-04 13:48:00 +0100

    refactor(Pokemon): remove unused variables.

--- src/app/models/pokemon.model.ts
+++ src/app/models/pokemon.model.ts
@@ -1,16 +1,5 @@
-import { PokemonSprite } from './pokemon-sprite.model';
-
 export interface Pokemon {
     id?: number;
     name: string;
     url: string;
-    abilities?: any[];
-    baseExperience?: number;
-    forms?: any;
-    moves?: any[];
-    sprites?: PokemonSprite;
-    stats?: any[];
-    height?: number;
-    weight?: number;
-    types?: any[];
 }

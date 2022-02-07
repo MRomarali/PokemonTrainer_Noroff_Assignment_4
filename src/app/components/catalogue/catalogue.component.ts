58793086b2bf0ecb6449dfb2e90961cf5688fbb3
Cusatelli <einarvandevelde@gmail.com>
2022-02-07 14:51:07 +0100

    fix: catalogue now correctly displayes caught pokemons.

--- src/app/components/catalogue/catalogue.component.ts
+++ src/app/components/catalogue/catalogue.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { API_POKE_URL, STORAGE_POKE_KEY } from 'src/app/constants';
+import { API_POKE_URL, STORAGE_POKE_KEY, STORAGE_TRAINER_KEY } from 'src/app/constants';
 import { clamp } from 'src/app/helpers/math.helper';
-import { getSessionStorage, setSessionStorage } from 'src/app/helpers/storage.helper';
+import { getLocalStorageAsString, getSessionStorage, setSessionStorage } from 'src/app/helpers/storage.helper';
 import { Pokemon } from 'src/app/models/pokemon.model';
+import { Trainer } from 'src/app/models/trainer.model';
 import { CollectionService } from 'src/app/service/collection.service';
 
 @Component({
@@ -13,10 +14,17 @@ import { CollectionService } from 'src/app/service/collection.service';
 export class CatalogueComponent implements OnInit {
   apiMaxLimit: number = 898;
   collection: Pokemon[] = [];
+  pokemonInTrainer: Pokemon[] = [];
+  pokemonId: number[] = [];
 
   constructor(private collectionService: CollectionService) { }
 
   ngOnInit(): void {
+    for (let index = 0; index < 900; index++) {
+      this.pokemonInTrainer.push({ id: -1, name: 'null', url: 'null' });
+    }
+    console.log(this.pokemonInTrainer);
+
     if (!getSessionStorage(STORAGE_POKE_KEY)) {
       this.apiGetPokemon(); // 1118 is max.
     } else {
@@ -29,29 +37,50 @@ export class CatalogueComponent implements OnInit {
     const collection = data ? data : 'null';
 
     this.collection = JSON.parse(collection).results;
+    const trainer = JSON.parse(getLocalStorageAsString(STORAGE_TRAINER_KEY) || 'null');
+    if (trainer.collection.length > 0) {
+      let posInTrainer = 0;
+      for (let index = 0; index < this.pokemonInTrainer.length; index++) {
+        const pokemon = trainer.collection[posInTrainer];
+
+        for (let innerIndex = 0; innerIndex < trainer.collection.length; innerIndex++) {
+            const pok = trainer.collection[innerIndex];
+            if(pok.id - 1 === index) {
+              this.pokemonInTrainer[index] = pok;
+            }
+        }
+      }
+    }
   }
 
   async apiGetPokemon(limit = this.apiMaxLimit, offset = 0) {
     limit = clamp(limit, 0, this.apiMaxLimit);
     offset = clamp(offset, 0, (this.apiMaxLimit - limit)); // subtract limit so it doesnt overflow max.
 
+    // 'Wrong' way to handle API req in Angular.
     await fetch(`${API_POKE_URL}/?limit=${limit}&offset=${offset}`)
       .then(response => response.json())
       .then(data => {
+        const trainer: Trainer = JSON.parse(getLocalStorageAsString(STORAGE_TRAINER_KEY) || 'null');
+        for (let index = 0; index < trainer.collection.length; index++) {
+          const pokemon = trainer.collection[index];
+          if (pokemon.id) {
+            const pokemonAPI = data.results[pokemon.id - 1];
+            data.results[pokemon.id - 1] = { id: pokemon.id, name: pokemonAPI.name, url: pokemonAPI.url, hasPokemon: pokemon.hasPokemon };
+          }
+        }
+
         setSessionStorage(STORAGE_POKE_KEY, data);
+
         this.update();
       });
   }
 
   public onAddClicked(id: number): void {
-    if(confirm(`Are you sure you wish to add ${this.capitalize(this.collection[id].name)}?`)) {
-      this.collection[id].id = id + 1; // Add id to pokemon before adding in collection. (Image url)
-      this.collectionService.addToCollection(this.collection[id]);
-    }
-  }
-
-  private capitalize(input: string): string {
-    const output = input[0].toUpperCase() + input.substring(1, input.length);
-    return output;
+    this.collection[id].id = id + 1; // Add id to pokemon before adding in collection. (Image url)
+    this.collectionService.addToCollection(this.collection[id]);
+    this.collection[id].hasPokemon = this.collectionService.hasPokemonInCollection(id);
+    
+    this.pokemonInTrainer[id] = this.collection[id];
   }
 }
